dce900bbf3875f5119c57444d2b62a32b375111c
LDI <az330015@gmail.com>
2021-08-11 10:29:25 +0900

    webSocket & api server with ilbo_sns

---
+++ src/main/java/webSocket/chatting/dao/UserDao.java
@@ -0,0 +1,18 @@
+package webSocket.chatting.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserDao {
+
+	String getUserNameById(String senderId);
+
+	ArrayList<String> getFollowers(int id);
+
+	ArrayList<String> getFollows(int id);
+
+	ArrayList<String> getGroupManagers(String gpNum);
+
+}
